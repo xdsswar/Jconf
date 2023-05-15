90fa6954f1e4fb479c97aaef483dee2f5bce2f03
xdsswar <xdsswar@gmail.com>
2023-05-15 01:23:34 -0400

    Initial Commit

--- src/test/java/xss/it/conf/JConfigTest.java
+++ src/test/java/xss/it/conf/JConfigTest.java
@@ -11,11 +11,10 @@ public class JConfigTest {
     @Test
     void testPlatform(){
         System.out.printf("Current OS : %s\n", Platform.getOS());
-        System.out.printf("Current OS is Windows : %s\n", Platform.getOS());
-        System.out.printf("Current OS is Mac OSX : %s\n", Platform.getOS());
-        System.out.printf("Current OS is Linux : %s\n", Platform.getOS());
-        System.out.printf("Current OS : %s\n", Platform.getOS());
-        System.out.printf("Current OS : %s\n", Platform.getOS());
+        System.out.printf("Current OS is Windows : %s\n", Platform.isWindows());
+        System.out.printf("Current OS is Mac OSX : %s\n", Platform.isMac());
+        System.out.printf("Current OS is Unix : %s\n", Platform.isUnix());
+        System.out.printf("Current OS is Solaris : %s\n", Platform.isSolaris());
     }
 
 }
